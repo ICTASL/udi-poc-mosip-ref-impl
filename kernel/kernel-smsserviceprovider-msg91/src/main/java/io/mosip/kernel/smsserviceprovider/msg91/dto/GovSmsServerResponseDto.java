fcfa755ed5be885e0e85238744a8665157186871
haren senevirathna <hsenevirathna@virtusa.com>
2021-07-28 17:13:54 +0530

    Govsms Intergration======== Govsms gateway intergrate to existing MOSIP platform

--- kernel/kernel-smsserviceprovider-msg91/src/main/java/io/mosip/kernel/smsserviceprovider/msg91/dto/GovSmsServerResponseDto.java
+++ kernel/kernel-smsserviceprovider-msg91/src/main/java/io/mosip/kernel/smsserviceprovider/msg91/dto/GovSmsServerResponseDto.java
@@ -1,18 +1,18 @@
-package io.mosip.kernel.smsserviceprovider.msg91.dto;
-
-import lombok.Data;
-
-/**
- * The DTO class for sms server response.
- * 
- * @author Ritesh Sinha
- * @since 1.0.0
- *
- */
-@Data
-public class GovSmsServerResponseDto {
-
-	private String message;
-	private Boolean status;
-	private String timestamp;
-}
+//package io.mosip.kernel.smsserviceprovider.msg91.dto;
+//
+//import lombok.Data;
+//
+///**
+// * The DTO class for sms server response.
+// *
+// * @author Ritesh Sinha
+// * @since 1.0.0
+// *
+// */
+//@Data
+//public class GovSmsServerResponseDto {
+//
+//	private String message;
+//	private Boolean status;
+//	private String timestamp;
+//}
