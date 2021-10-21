d1432fcec05bf4ca9b2b31123ad7a1786a439f9d
Hansana Herath <subodhahansana@gmail.com>
2021-10-21 19:09:46 +0530

    https://mosip.atlassian.net/servicedesk/customer/portal/5/SSD-116 Fixed

--- admin-ui/src/app/features/resources/users/view/view.component.ts
+++ admin-ui/src/app/features/resources/users/view/view.component.ts
@@ -10,7 +10,7 @@ import { AppConfigService } from 'src/app/app-config.service';
 })
 export class ViewComponent implements OnInit {
   keycloakUrl =
-    'https://mosipkeycloak.southindia.cloudapp.azure.com/auth/admin/master/console/#/realms/mosip/users';
+    'https://testsludi.lgcc.gov.lk/keycloak/auth/admin/master/console/#/realms/mosip/users';
 
   constructor(
     private auditService: AuditService,
